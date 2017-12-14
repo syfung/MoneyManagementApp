22b0953152d5f756601fb8bc750fecbd95393f70
josh <josh@localhost.localdomain>
2017-12-14 13:46:59 -0500

    server useage

--- src/bankClient/BankClient.java
+++ src/bankClient/BankClient.java
@@ -4,7 +4,11 @@ public class BankClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		if(args.length == 2) {
+         
+        } else {
+            System.out.println("Useage: BankServer hostName port");
+        }
 	}
-
+	
 }
