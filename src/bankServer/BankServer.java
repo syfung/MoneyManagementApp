805abef65744f808d3cabbd1729d758d14c2362b
josh <josh@localhost.localdomain>
2017-12-14 13:56:42 -0500

    accept client connection, Thread to handle connection

--- src/bankServer/BankServer.java
+++ src/bankServer/BankServer.java
@@ -1,9 +1,30 @@
 package bankServer;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class BankServer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if (args.length == 1) {
+			int portNumber = Integer.valueOf(args[0]);
+
+			try {
+				ServerSocket serverSocket = new ServerSocket(portNumber);
+				while(true) {
+					Socket clientSocket = serverSocket.accept();
+					
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} else {
+			System.out.println("Useage: BankServer hostName port");
+		}
 
 	}
 
---
+++ src/bankServer/BankServerThread.java
@@ -0,0 +1,14 @@
+package bankServer;
+
+import java.net.Socket;
+
+public class BankServerThread implements Runnable {
+	Socket clientSocket;
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
